d2f138724a35976f612e046810b9d71dd6a9e972
Nandor Krizbai <krizbai.nandor@gmail.com>
2026-04-17 14:34:37 +0200

    Split Match tests into 2 files

--- tests/EFCore.ParadeDB.PgSearch.IntegrationTests/MatchDisjunctionTests.cs
+++ tests/EFCore.ParadeDB.PgSearch.IntegrationTests/MatchDisjunctionTests.cs
@@ -3,7 +3,7 @@ using Shouldly;
 
 namespace EFCore.ParadeDB.PgSearch.IntegrationTests;
 
-public sealed class MatchTests : TestBase
+public sealed class MatchDisjunctionTests : TestBase
 {
     [Test]
     public async Task MatchDisjunction_ExecutesSuccessfully()
@@ -95,102 +95,6 @@ public sealed class MatchTests : TestBase
         var results = await context
             .Products.Where(p =>
                 EF.Functions.MatchDisjunction(p.Description, "these", Pdb.Fuzzy(2), Pdb.Boost(2.3f))
-            )
-            .ToListAsync();
-
-        results.ShouldNotBeNull();
-    }
-
-    [Test]
-    public async Task MatchConjunction_ExecutesSuccessfully()
-    {
-        await using var context = DbFixture.CreateContext();
-
-        var results = await context
-            .Products.Where(p => EF.Functions.MatchConjunction(p.Description, "these"))
-            .ToListAsync();
-
-        results.ShouldNotBeNull();
-    }
-
-    [Test]
-    public async Task MatchConjunction_WithInlineArray_ExecutesSuccessfully()
-    {
-        await using var context = DbFixture.CreateContext();
-
-        var results = await context
-            .Products.Where(p =>
-                EF.Functions.MatchConjunction(p.Description, new[] { "these", "shoes" })
-            )
-            .ToListAsync();
-
-        results.ShouldNotBeNull();
-    }
-
-    [Test]
-    public async Task MatchConjunction_WithArrayVariable_ExecutesSuccessfully()
-    {
-        await using var context = DbFixture.CreateContext();
-
-        string[] terms = ["these", "shoes"];
-
-        var results = await context
-            .Products.Where(p => EF.Functions.MatchConjunction(p.Description, terms))
-            .ToListAsync();
-
-        results.ShouldNotBeNull();
-    }
-
-    [Test]
-    public async Task MatchConjunction_WithFuzzy_ExecutesSuccessfully()
-    {
-        await using var context = DbFixture.CreateContext();
-
-        var results = await context
-            .Products.Where(p =>
-                EF.Functions.MatchConjunction(p.Description, "these", Pdb.Fuzzy(2))
-            )
-            .ToListAsync();
-
-        results.ShouldNotBeNull();
-    }
-
-    [Test]
-    public async Task MatchConjunction_WithBoost_ExecutesSuccessfully()
-    {
-        await using var context = DbFixture.CreateContext();
-
-        var results = await context
-            .Products.Where(p =>
-                EF.Functions.MatchConjunction(p.Description, "these", Pdb.Boost(2.3f))
-            )
-            .ToListAsync();
-
-        results.ShouldNotBeNull();
-    }
-
-    [Test]
-    public async Task MatchConjunction_WithConst_ExecutesSuccessfully()
-    {
-        await using var context = DbFixture.CreateContext();
-
-        var results = await context
-            .Products.Where(p =>
-                EF.Functions.MatchConjunction(p.Description, "these", Pdb.Const(20.3f))
-            )
-            .ToListAsync();
-
-        results.ShouldNotBeNull();
-    }
-
-    [Test]
-    public async Task MatchConjunction_WithFuzzyAndBoost_ExecutesSuccessfully()
-    {
-        await using var context = DbFixture.CreateContext();
-
-        var results = await context
-            .Products.Where(p =>
-                EF.Functions.MatchConjunction(p.Description, "these", Pdb.Fuzzy(2), Pdb.Boost(2.3f))
             )
             .ToListAsync();
 
